b9e300a920919d69f660afef4c19b5204c568f48
Seiji Tada <Seiji.Tada@seagate.com>
2015-11-23 21:40:40 +0900

    init.el added

---
+++ init.el
@@ -0,0 +1,19 @@
+(keyboard-translate ?\C-h ?\C-?)
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(package-initialize)
+
+(set-default-coding-systems 'utf-8)
+(set-buffer-file-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+
+(set-language-environment "Japanese")
+(when (require 'skk nil t)
+  (global-set-key (kbd "C-x j") 'skk-auto-fill-mode)
+  (setq default-input-method "japanese-skk")
+  (require 'skk-study))
+
+(setenv "PATH" (concat "/usr/local/smlnj/bin:" (getenv "PATH")))
+(setq exec-path (cons "/usr/local/smlnj/bin"  exec-path))
