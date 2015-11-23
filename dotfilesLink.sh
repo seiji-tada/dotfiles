24443c16d370ee148cc7e3b59cba3f78916d9a52
Seiji Tada <Seiji.Tada@seagate.com>
2015-11-23 17:41:58 +0900

    removed files related to vim

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,7 +1,4 @@
 #! /bin/bash
-ln -sf ~/dotfiles/_vimrc ~/.vimrc
-ln -sf ~/dotfiles/_gvimrc ~/.gvimrc
-ln -sf ~/dotfiles/colors ~/.vim/colors
 ln -sf ~/dotfiles/_bashrc ~/.bashrc
 ln -sf ~/dotfiles/_bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/_emacs_el ~/.emacs.el
